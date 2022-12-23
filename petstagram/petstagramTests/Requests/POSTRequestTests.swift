d58c469018a0eba950a28b0b29424e1c4c0960e9
Jess Lê <jessicale90@gmail.com>
2022-12-23 13:15:14 -0600

    Adds PostRequest test bad data error handling

--- petstagram/petstagramTests/Requests/POSTRequestTests.swift
+++ petstagram/petstagramTests/Requests/POSTRequestTests.swift
@@ -9,9 +9,8 @@ import XCTest
 @testable import petstagram
 
 class POSTRequestTests: XCTestCase {
-    func test_handle_data_returns_decodedPost() {
+    func test_handle_validData_returns_decodedPost() {
         let data = JsonData.validResponse.data(using: .utf8)!
-        let expected = PostData.posts
 
         let request = POSTRequest(method: .POST, path: "")
         var actual = [Post]()
@@ -22,6 +21,21 @@ class POSTRequestTests: XCTestCase {
             XCTFail(error.localizedDescription)
         }
 
-        XCTAssertEqual(actual, expected)
+        XCTAssertEqual(actual, PostData.posts)
+    }
+
+    func test_handle_invalidData_throwsError() {
+        let data = JsonData.invalidResponse.data(using: .utf8)!
+
+        let request = POSTRequest(method: .POST, path: "")
+        var actual = [Post]()
+
+        do {
+            actual = try request.handle(response: data)
+        } catch let error {
+            XCTAssertEqual(error.localizedDescription, "The data couldn’t be read because it isn’t in the correct format.")
+        }
+
+        XCTAssertEqual(actual, [])
     }
 }
